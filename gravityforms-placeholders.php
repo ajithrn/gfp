8b6e24d92a8a6a69b1a0e4c8170c5ba029332d01
Ajith R Nair <trytoinnovate@gmail.com>
2014-02-25 19:47:18 +0530

    Update gravityforms-placeholders.php

--- gravityforms-placeholders.php
+++ gravityforms-placeholders.php
@@ -8,14 +8,12 @@ Just add a "gplaceholder" CSS classname to the required fields or form, it will 
 */
 
 if ( isset( $GLOBALS['pagenow'] ) && $GLOBALS['pagenow'] == 'wp-login.php' )
-
 	return;
 
 add_action('wp_print_scripts', 'gf_placeholder_enqueue_scripts');
 
 function gf_placeholder_enqueue_scripts() {
-
 	$plugin_url =  get_template_directory_uri() . '/assets/plugins/gfp';
-	echo "<script>var jquery_placeholder_url = '" . $plugin_url . "/jquery.placeholder-1.0.1.js';</script>";
+	echo "<script>var jquery_placeholder_url = '" . $plugin_url . "/jquery.placeholder.js';</script>";
 	wp_enqueue_script('_gf_placeholders', $plugin_url . '/gf.placeholders.js', array('jquery'), '1.0' );
 }
